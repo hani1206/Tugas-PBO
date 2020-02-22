4a1e94cc2eca03d4e719d58c15d8384cb5c68c4d
hani1206 <55271058+hani1206@users.noreply.github.com>
2020-02-23 02:35:31 +0700

    Update Bola.java

--- Tugas2/src/bangunDatar1/Bola.java
+++ Tugas2/src/bangunDatar1/Bola.java
@@ -3,7 +3,6 @@ package bangunDatar1;
 public class Bola extends Lingkaran {
     @Override
     public double lingLuas(double r) {
-        //return super.lingkaranLuas()
         double volume = ((4*3.14 * r * r)/3);
 
         return volume;
